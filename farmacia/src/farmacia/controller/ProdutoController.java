13b6289548e39ae252e31c813ddc32aade980c13
Victor Hugo <projektvek@gmail.com>
2025-05-03 15:03:00 -0300

    :Sparkles: Criado CRUD

--- farmacia/src/farmacia/controller/ProdutoController.java
+++ farmacia/src/farmacia/controller/ProdutoController.java
@@ -1,5 +1,37 @@
 package farmacia.controller;
 
-public class ProdutoController {
+import farmacia.repository.ProdutoRepository;
+
+public class ProdutoController implements ProdutoRepository {
+
+	@Override
+	public boolean criaProduto() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void listaProdutos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean consultaProduto() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean atualizaProduto() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean deletaProduto() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }
